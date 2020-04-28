317433a8ee79f25cafd2d51ab6c88e6c39b4307a
Ayaan Khan <ayaankhan98@gmail.com>
2020-04-29 00:08:25 +0530

    Added screen.h

---
+++ screen.h
@@ -0,0 +1,25 @@
+#ifndef SCREEN_H_
+#define SCREEN_H_
+
+#include <SDL2/SDL.h>
+
+namespace screenComponents {
+
+  class Screen {
+    public:
+      const static int SCREEN_WIDTH = 800;
+      const static int SCREEN_HEIGHT = 600;
+    private:
+      SDL_Window *m_window;
+      SDL_Renderer *m_renderer;
+      SDL_Texture *m_texture;
+      Uint32 *m_buffer;
+    public:
+      Screen();
+      bool initScreen();
+      bool processEvent();
+      void destroyScreen();
+  };
+};
+
+#endif
